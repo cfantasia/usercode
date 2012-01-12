d6a8164dd09d70bdec676e07bdbcf8e1ae634973
fladias <>
2012-01-12 03:17:49 +0000

    file

--- CMSDAS_WZ_2012/bin/analyzeWprimeWZ_cfg.py
+++ CMSDAS_WZ_2012/bin/analyzeWprimeWZ_cfg.py
@@ -77,7 +77,7 @@ process.WprimeAnalyzer = cms.PSet(
           description = cms.untracked.string("Wprime500"),
           x_section = cms.untracked.double(0.879),
           Nprod_evt = cms.untracked.int32(1221134),
-          subdir = cms.untracked.string("/pnfs/cms/WAX/11/store/user/fladias/MCPatOmnilepton-W06-06-05A-WprimeToWZTo3LNu_emt_M-500_7TeV-pythia6/"),
+          subdir = cms.untracked.string("dcache:/pnfs/cms/WAX/11/store/user/fladias/MCPatOmnilepton-W06-06-05A-WprimeToWZTo3LNu_emt_M-500_7TeV-pythia6/"),
           pathname = cms.string("filelists/MCPatOmnilepton-W06-06-05A-WprimeToWZTo3LNu_emt_M-500_7TeV-pythia6.txt"),
         ),
       ),#end of files
