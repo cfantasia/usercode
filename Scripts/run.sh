2b8ab814030ad04817fca99c2cd1359adff89cbe
fantasia <>
2012-03-16 13:42:28 +0000

    for christos

--- Scripts/run.sh
+++ Scripts/run.sh
@@ -41,30 +41,39 @@ endif
 # endif 
 # echo $ProcessPadded
 
+set OutFile=Sample${Process}_${Mode}.txt
+#set OutFile=${Mode}_Sample${Process}.txt
+
 switch ($Mode)
-    case "WZMatrix": #Matrix Method
-        time WPrimeAnalyzer UserCode/CMGWPrimeGroup/bin/analyzeWprimeWZMatrix_cfg.py ${Process} >& Sample${Process}_WprimeWZMatrix.txt
-        breaksw
     case "WprimeWZ": #Wprime WZ
-        time WPrimeAnalyzer UserCode/CMGWPrimeGroup/bin/analyzeWprimeWZ_cfg.py ${Process} >& Sample${Process}_WprimeWZ.txt
+        time WPrimeAnalyzer UserCode/CMGWPrimeGroup/bin/analyzeWprimeWZ_cfg.py ${Process} >& $OutFile
         breaksw
     case "HadVZ": #Had VZ
-        time WPrimeAnalyzer UserCode/CMGWPrimeGroup/bin/analyzeWprimeHadVZ_cfg.py ${Process} >& Sample${Process}_HadVZAnalyzer.txt
+        time WPrimeAnalyzer UserCode/CMGWPrimeGroup/bin/analyzeWprimeHadVZ_cfg.py ${Process} >& $OutFile
         breaksw
     case "HadVW": #Had WZ
-        time WPrimeAnalyzer UserCode/CMGWPrimeGroup/bin/analyzeWprimeHadVW_cfg.py ${Process} >& Sample${Process}_HadVW.txt
+        time WPrimeAnalyzer UserCode/CMGWPrimeGroup/bin/analyzeWprimeHadVW_cfg.py ${Process} >& $OutFile
         breaksw
     case "TB": #TB
-        time WPrimeAnalyzer UserCode/CMGWPrimeGroup/bin/analyzeWprimeTB_cfg.py ${Process} >& Sample${Process}_TB.txt
+        time WPrimeAnalyzer UserCode/CMGWPrimeGroup/bin/analyzeWprimeTB_cfg.py ${Process} >& $OutFile
+        breaksw
+    case "EWKWZ": #EWK WZ
+        time WPrimeAnalyzer UserCode/CMGWPrimeGroup/bin/analyzeEWKWZ_cfg.py ${Process} >& $OutFile
+        breaksw
+    case "WZMatrix": #Matrix Method
+        time WPrimeAnalyzer UserCode/CMGWPrimeGroup/bin/analyzeWprimeWZMatrix_cfg.py ${Process} >& $OutFile
         breaksw
     case "WZEffRate": #Eff Rate
-        time WPrimeAnalyzer UserCode/CMGWPrimeGroup/bin/analyzeWZEffRate_cfg.py ${Process} >& Sample${Process}_WZEffRate.txt
+        time WPrimeAnalyzer UserCode/CMGWPrimeGroup/bin/analyzeWZEffRate_cfg.py ${Process} >& $OutFile
         breaksw
     case "WZElecFakeRate": #Elec Fake Rate
-        time WPrimeAnalyzer UserCode/CMGWPrimeGroup/bin/analyzeWZFakeRate_cfg.py ${Process} >& Sample${Process}_WZFakeRateElec.txt
+        time WPrimeAnalyzer UserCode/CMGWPrimeGroup/bin/analyzeWZFakeRate_cfg.py ${Process} >& $OutFile
         breaksw
     case "WZMuonFakeRate": #Muon Fake Rate
-        time WPrimeAnalyzer UserCode/CMGWPrimeGroup/bin/analyzeWZFakeRate_cfg.py ${Process} >& Sample${Process}_WZFakeRateMuon.txt
+        time WPrimeAnalyzer UserCode/CMGWPrimeGroup/bin/analyzeWZFakeRate_cfg.py ${Process} >& $OutFile
+        breaksw
+    case "TTbar": #HadVZ TTbar check
+        time WPrimeAnalyzer UserCode/CMGWPrimeGroup/bin/analyzeTTbar_cfg.py ${Process} >& $OutFile
         breaksw
     default:
         echo "You didn't choose a valid mode: " $Mode
